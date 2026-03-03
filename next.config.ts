105896e80390b4585efe3a8182af271fca01a5a3
Rohit Ramaprasad <rramaprasad@adobe.com>
2026-03-02 23:57:17 -0800

    some fixes

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: process.env.PAGES_BASE_PATH,
   images: {
     unoptimized: true,
   },
-  // If deploying to GitHub Pages with a repo name (e.g., username.github.io/repo-name),
-  // uncomment and set basePath and assetPrefix:
-  // basePath: '/repo-name',
-  // assetPrefix: '/repo-name',
 };
 
 export default nextConfig;
